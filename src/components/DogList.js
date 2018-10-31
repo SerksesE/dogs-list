e3e280993a73fdfd543158da5f3da471b7d5eda6
SerksesE <serkses@hotmail.com>
2018-10-31 09:47:06 +0100

    added DogList component

---
+++ src/components/DogList.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class DogList extends Component {
+  render() {
+    return (
+      <div className="dog-list">
+        <h1>Dog List</h1>
+      </div>
+    )
+  }
+}
