44206a5f56a1215c678cbf3b0e338b7ee72dcb80
SerksesE <serkses@hotmail.com>
2018-10-31 12:10:28 +0100

    progress

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <DogList />
+        <main>
+          <DogList />
+        </main>
       </div>
     );
   }
